77229fc79bc09b5ef24f0dd739e19974845f7dd1
baek85 <baeksh@kaist.ac.kr>
2019-05-09 23:16:59 +0900

    baek0509

--- train.sh
+++ train.sh
@@ -10,7 +10,7 @@
 # k_obsrv -> the number of samples for sparse observation 
 # save_image -> save the inner plots in save_dir folder
 
-## RNN-base
+## RNN-base SGD 1.0 unstable / ADAM 1e-4 converge slow
 #CUDA_VISIBLE_DEVICES=0 python main.py --meta_model rnn --optim sgd --lr 1.0 --save_dir RNN_base_sgd_1.0
 #CUDA_VISIBLE_DEVICES=0 python main.py --meta_model rnn --optim sgd --lr 1.0 --save_dir RNN_base_sgd_1.0_2
 #CUDA_VISIBLE_DEVICES=1 python main.py --meta_model rnn --optim sgd --lr 0.1 --save_dir RNN_base_sgd_0.1
@@ -19,7 +19,7 @@
 #CUDA_VISIBLE_DEVICES=2 python main.py --meta_model rnn --optim adam --lr 1.0 --save_dir RNN_base_adam_1.0_2
 #CUDA_VISIBLE_DEVICES=3 python main.py --meta_model rnn --optim adam --lr 1e-4 --save_dir RNN_base_adam_1e-4
 #CUDA_VISIBLE_DEVICES=3 python main.py --meta_model rnn --optim adam --lr 0.1 --save_dir RNN_base_adam_0.1_2
-## MLP Not masking
+## MLP Not masking SGD 1.0 / ADAM 1e-3 unstable
 CUDA_VISIBLE_DEVICES=0 python main.py --meta_model ours --not_masking --optim sgd --lr 1.0 --save_dir MLP_single_nomask_sgd_1.0
 #CUDA_VISIBLE_DEVICES=0 python main.py --meta_model ours --not_masking --optim sgd --lr 1.0 --save_dir MLP_single_nomask_sgd_1.0_2
 CUDA_VISIBLE_DEVICES=0 python main.py --meta_model ours --not_masking --optim sgd --lr 0.1 --save_dir MLP_single_nomask_sgd_0.1
@@ -28,7 +28,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py --meta_model ours --not_masking --optim ad
 #CUDA_VISIBLE_DEVICES=0 python main.py --meta_model ours --not_masking --optim adam --lr 1e-3 --save_dir MLP_single_nomask_adam_1e-3_2
 CUDA_VISIBLE_DEVICES=1 python main.py --meta_model ours --not_masking --optim adam --lr 1e-4 --save_dir MLP_single_nomask_adam_1e-4
 #CUDA_VISIBLE_DEVICES=1 python main.py --meta_model ours --not_masking --optim adam --lr 1e-4 --save_dir MLP_single_nomask_adam_1e-4_2
-## MLP masking
+## MLP masking SGD 1.0 / ADAM 1e-3 unstable
 CUDA_VISIBLE_DEVICES=0 python main.py --meta_model ours --optim sgd --lr 1.0 --save_dir MLP_single_mask_sgd_1.0
 #CUDA_VISIBLE_DEVICES=0 python main.py --meta_model ours --optim sgd --lr 1.0 --save_dir MLP_single_mask_sgd_1.0_2
 CUDA_VISIBLE_DEVICES=1 python main.py --meta_model ours --optim sgd --lr 0.1 --save_dir MLP_single_mask_sgd_0.1
