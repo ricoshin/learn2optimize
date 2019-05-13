c1bc7cfa0c413ac19de2657e330406f94f472894
whshin <rico.shin@gmail.com>
2019-05-13 19:28:26 +0900

    normalize KLD for obsrv_multi (works well)

--- optimizers/obsrv_multi.py
+++ optimizers/obsrv_multi.py
@@ -137,7 +137,7 @@ class Optimizer(OptimizerBase):
       with WalltimeChecker(walltime if mode == 'train' else None):
         model_test = C(model_cls(params=params))
         test_nll, test_acc = utils.isnan(*model_test(*data['in_test'].load()))
-        test_kld = kld / data['in_test'].full_size
+        test_kld = kld / data['in_test'].full_size / unroll
         ## kl annealing function 'linear' / 'logistic' / None
         test_kld2 = test_kld * kl_anneal_function(
           anneal_function=None, step=iter, k=0.0025, x0=optim_it)
